e04af29bd4e9fd64673c61a7c190a55ab3db87b2
FridmanVadim <fridmanxk3@gmail.com>
2020-02-08 23:09:23 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var mySwiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.arrow',
+  },
+
+  breakpoints: {
+    // when window width is >= 640px
+    540: {
+      slidesPerView: 2,
+    }
+  }
+
+});
+
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+})
